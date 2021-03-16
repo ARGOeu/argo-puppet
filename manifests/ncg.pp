ecaf012113611e3a8d412956d73138f4290e4a84
Katarina Zailac <katarina.zailac@gmail.com>
2021-03-16 11:42:36 +0100

    manifest for argo-ncg

---
+++ manifests/ncg.pp
@@ -0,0 +1,41 @@
+class argo::mon::ncg (
+  $nagioshost = '',
+  $nagiosadmin = '',
+  $webapi_url = '',
+  $webapi_token = '',
+  $poem_url = '',
+  $poem_token = '',
+  $profiles = '',
+  $conf_source = 'puppet:///private/ncg/ncg.conf',
+  $localdb = false,
+  $version = latest,
+) {
+  File {
+    ensure => present,
+    owner  => root,
+    group  => root,
+    mode   => '0644',
+  }
+
+  package { 'argo-ncg':
+    ensure => $version,
+  }
+
+  file { '/etc/argo-ncg/ncg-vars.conf':
+    content => template('templates/ncg-vars.conf.erb'),
+    require => Package['argo-ncg'],
+  }
+
+  file { '/etc/argo-ncg/ncg.conf':
+    source  => $conf_source,
+    require => Package['argo-ncg'],
+  }
+
+  if ( $localdb ) {
+    file { '/etc/argo-ncg/ncg-localdb.d':
+      ensure  => directory,
+      recurse => remote,
+      source  => 'puppet:///private/ncg/ncg-localdb.d/',
+    }
+  }
+}
