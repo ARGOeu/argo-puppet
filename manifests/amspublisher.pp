03dc6f78c6f85a4e4e4d9166b8bd04681056207f
Katarina Zailac <katarina.zailac@gmail.com>
2021-03-16 11:45:52 +0100

    manifest for argo-ams-publisher

---
+++ manifests/amspublisher.pp
@@ -0,0 +1,19 @@
+class argo::mon::amspublisher (
+  $nagioshost = '',
+  $publisher_queues_topics = {},
+) {
+  package {'argo-nagios-ams-publisher':
+    ensure => latest,
+  }
+
+  file { '/etc/argo-nagios-ams-publisher/ams-publisher.conf':
+    content => template('templates/ams-publisher.conf.erb'),
+    notify  => Service['ams-publisher.service'],
+  }
+
+  service { 'ams-publisher.service': 
+    ensure  => 'running',
+    enable  => true,
+    require => [ Package['argo-nagios-ams-publisher'], File['/etc/argo-nagios-ams-publisher/ams-publisher.conf'] ],
+  }
+}
