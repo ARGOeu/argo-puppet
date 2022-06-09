89daa7645fa514c1330abcbbfdd5e285855f9579
Katarina Zailac <katarina.zailac@gmail.com>
2022-06-09 15:49:54 +0200

    include argo-probe-poem to required packages for internal monitoring

--- manifests/mon/internal.pp
+++ manifests/mon/internal.pp
@@ -12,7 +12,7 @@ class argo::mon::internal (
     mode   => '0644'
   }
 
-  package { ['nagios-plugins-ping', 'nagios-plugins-igtf', 'nagios-plugins-nrpe', 'nagios-plugins-argo', 'argo-probe-nagios']:
+  package { ['nagios-plugins-ping', 'nagios-plugins-igtf', 'nagios-plugins-nrpe', 'nagios-plugins-argo', 'argo-probe-nagios', 'argo-probe-poem']:
   }
 
   file { '/etc/argo-ncg/ncg.argo-public-devel':
