a2b89d6ce062951a38e1f299f7c0f4c84dbb900a
Katarina Zailac <katarina.zailac@gmail.com>
2023-01-30 10:14:53 +0100

    Add secrets_file in argo::mon::sensu manifest

--- manifests/mon/sensu.pp
+++ manifests/mon/sensu.pp
@@ -1,9 +1,20 @@
 class argo::mon::sensu (
-  $agent   = false,
-  $backend = false,
-  $tenants = [],
+  $agent        = false,
+  $backend      = false,
+  $secrets_file = '',
+  $tenants      = [],
 ) {
   include ::yum::repo::umd4
+  include ::yum::repo::srce::intern
+
+  if ($secrets_file) {
+    file { '/etc/sensu/secret_envs':
+      ensure => present,
+      owner  => 'sensu',
+      group  => 'sensu',
+      source => $secrets_file,
+    }
+  }
 
   if ($agent) {
     include sensu::agent
