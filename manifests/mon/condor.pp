c1e0ea339e113dece3c4a924a4aa95adb976ec51
Katarina Zailac <katarina.zailac@gmail.com>
2024-01-04 15:27:38 +0100

    Create condor.pp manifest

---
+++ manifests/mon/condor.pp
@@ -0,0 +1,19 @@
+class argo::mon::condor (
+  $local_config='puppet:///modules/argo/mon/condor/condor_config.local'
+) {
+
+  File {
+    ensure => present,
+    owner  => root,
+    group  => root,
+    mode   => '0644',
+  }
+  
+  package { 'condor': 
+  }
+
+  file { '/etc/condor/condor_config.local':
+    source  => $local_config,
+    require => Package['condor'],
+  }
+}
