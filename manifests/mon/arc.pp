f35190002f93c384fa6ab470e3aa6b8f7ff3e0db
Katarina Zailac <katarina.zailac@gmail.com>
2024-01-15 13:42:10 +0100

    Create separate arc manifest

---
+++ manifests/mon/arc.pp
@@ -0,0 +1,20 @@
+class argo::mon::arc (
+  $local_ini='puppet:///modules/argo/mon/egi/90-local.ini',
+) {
+  include yum::repo::nordugrid
+
+  File {
+    ensure => present,
+    owner  => root,
+    group  => root,
+    mode   => '0644',
+  }
+
+  package { ['nordugrid-arc-nagios-plugins-egi', 'argo-probe-igtf']:
+  }
+
+  file { '/etc/arc/nagios/90-local.ini':
+    source  => $local_ini,
+    require => Package['nordugrid-arc-nagios-plugins-egi'],
+  }
+}
