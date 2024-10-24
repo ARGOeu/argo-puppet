b02d31451d997284398bf297e88c51fe95e6fc59
Katarina Zailac <katarina.zailac@gmail.com>
2024-10-24 13:00:22 +0200

    Remove condition from disable_ipv6 manifest

--- manifests/mon/disable_ipv6.pp
+++ manifests/mon/disable_ipv6.pp
@@ -1,13 +1,11 @@
 class argo::mon::disable_ipv6 () {
-  unless $argo::mon::disable_ipv6 == false {
-    sysctl { 'net.ipv6.conf.all.disable_ipv6' :
-      value   => '1',
-      comment => 'Disable IPv6',
-    }
+  sysctl { 'net.ipv6.conf.all.disable_ipv6' :
+    value   => '1',
+    comment => 'Disable IPv6',
+  }
 
-    sysctl { 'net.ipv6.conf.default.disable_ipv6' :
-      value   => '1',
-      comment => 'Disable IPv6',
-    }
+  sysctl { 'net.ipv6.conf.default.disable_ipv6' :
+    value   => '1',
+    comment => 'Disable IPv6',
   }
 }
