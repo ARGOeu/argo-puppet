3c4d97f429607448f764b7d203306adaba61c56e
Katarina Zailac <katarina.zailac@gmail.com>
2024-10-25 12:44:41 +0200

    Explicitly disable ipv6 in argo::mon manifest

--- manifests/mon.pp
+++ manifests/mon.pp
@@ -8,9 +8,13 @@ class argo::mon (
   $sensu         = false,
   $condor        = false,
   $arc           = false,
+  $disable_ipv6  = false,
 ) {
   include yum::repo::argo
-  include argo::mon::disable_ipv6
+
+  if ($disable_ipv6) {
+    include argo::mon::disable_ipv6
+  }
 
   if ($egi) {
     include ::yum::repo::umd4
