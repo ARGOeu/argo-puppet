3f919c739582032817e783ae438b083d01388313
Katarina Zailac <katarina.zailac@gmail.com>
2025-02-13 11:20:42 +0100

    Option to skip repo configuration and package installation

--- manifests/mon/condor.pp
+++ manifests/mon/condor.pp
@@ -1,5 +1,6 @@
 class argo::mon::condor (
-  $local_config='puppet:///modules/argo/mon/condor/condor_config.local'
+  $local_config='puppet:///modules/argo/mon/condor/condor_config.local',
+  $handle_package=true
 ) {
 
   File {
@@ -9,14 +10,16 @@ class argo::mon::condor (
     mode   => '0644',
   }
 
-  if (Integer($facts['os']['release']['major']) > 7) {
-    file { '/etc/yum.repos.d/htcondor.repo': 
-      source => 'puppet:///modules/argo/mon/condor/htcondor.repo',
-      mode   => '0644',
+  if ($handle_package) {
+    if (Integer($facts['os']['release']['major']) > 7) {
+      file { '/etc/yum.repos.d/htcondor.repo': 
+        source => 'puppet:///modules/argo/mon/condor/htcondor.repo',
+        mode   => '0644',
+      }
     }
-  }
-  
-  package { 'condor': 
+    
+    package { 'condor': 
+    }
   }
 
   file { '/etc/condor/condor_config.local':
